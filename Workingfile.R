5c2708fd3428abd85c8dd97586fd5699b0af614a
juhopjs <44612736+juhopjs@users.noreply.github.com>
2018-11-11 22:32:27 +0200

    1

--- Workingfile.R
+++ Workingfile.R
@@ -11,7 +11,7 @@
 
 #Juho Saari
 #11.11.2018
-#Dataset for IODS 2018 exercises.
+#Dataset for IODS exercises.
 
 learning2014 <- read_delim("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
 
@@ -20,3 +20,6 @@ Write your name, date and a one sentence file description as
 a comment on the top of the script file. Save the script for example
 as 'create_learning2014.R' in the 'data' folder.
 Complete the rest of the steps in that script.
+
+library(GGally)
+library(ggplot2)
